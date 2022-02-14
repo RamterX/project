d3d7cfe741a8c4107b8c8517c573865883d04f8a
Vlad <chava.vlad@gmail.com>
2022-02-14 14:43:50 +0300

    lab2

---
+++ lab/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+
+    public class Address
+    {
+        private string index;
+
+
+        private string country;
+
+        private string city;
+        private string street;
+        private string house;
+        private string apartment;
+
+        public string Index { get => index; set => index = value; }
+        public string Country { get => country; set => country = value; }
+        public string City { get => city; set => city = value; }
+        public string Street { get => street; set => street = value; }
+        public string House { get => house; set => house = value; }
+        public string Apartment { get => apartment; set => apartment = value; }
+    }
+}
--- lab/Program.cs
+++ lab/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using lab;
+
+namespace lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.City = "kiyv";
+            address.Index = "02192";
+            address.Country = "Ukrainian";
+            address.Street = "Miropolskaya";
+            address.House = "37";
+            address.Apartment = "8";
+
+
+
+
+
+            Console.WriteLine(address.City);
+            Console.WriteLine(address.Index);
+            Console.WriteLine(address.Country);
+            Console.WriteLine(address.Street);
+            Console.WriteLine(address.House);
+            Console.WriteLine(address.Apartment);
+         }
+    }
+ }
+
+
